693d9bebf13eb0ce9062fba132a88596fbabf7be
Valentin Gunoyev <vgunoyev@dar.io>
2025-01-28 17:02:51 +0500

    feat:lang

---
+++ lang/en/local_quickstats.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Languages configuration for the local_quickstats plugin.
+ *
+ * @package   local_quickstats
+ * @copyright 2025, Valentin Gunoyev <valentin.gunoyev@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'quickstats';
+$string['enable'] = 'Enable';
+$string['enable_desc'] = 'Is plugin enabled';
+$string['period'] = 'Number of days';
+$string['period_desc'] = 'Number of days (N) for which a user is considered “active” (for example,
+users who logged in within the last N days)';
