0bac359cbe624cbcff751dd6c2170bddaea9492e
Valentin Gunoyev <vgunoyev@dar.io>
2025-02-09 20:41:57 +0500

    feat: chart js src

--- amd/src/chart.js
+++ amd/src/chart.js
@@ -1,28 +1,22 @@
-// amd/src/chart.js
-
-define(['core/ajax', 'core/notification', 'chartjs'], function (ajax, notification, Chart) {
+define(['local_quickstats/lib/chart.umd'], function(Chart) {
     return {
-        init: function (data) {
-            var ctx = document.getElementById('myChart').getContext('2d');
-            var chart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: data.labels,
-                    datasets: [{
-                        label: 'Active Users',
-                        data: data.counts,
-                        borderColor: 'rgba(75, 192, 192, 1)',
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    responsive: true,
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
+        init: function (params) {
+            const data = JSON.parse(params);
+            new Chart(
+                document.getElementById('chart'),
+                {
+                    type: 'bar',
+                    data: {
+                        labels: data.map(row => row.year),
+                        datasets: [
+                            {
+                                label: 'User count by day',
+                                data: data.map(row => row.count)
+                            }
+                        ]
                     }
-                });
+                }
+            );
         }
-        };
-    });
+    };
+});
